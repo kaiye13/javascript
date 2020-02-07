19f9dc815af240aff0163d2065bf2a01f45e4d3a
kaiye13 <kaatschillemans@hotmail.com>
2020-02-07 14:02:42 +0100

    oefening 4

---
+++ oefening4.js
@@ -0,0 +1,8 @@
+var jaar = 2020;
+alert("Het jaar is: "+jaar);
+jaar = jaar-19;
+alert("Sommigen zijn geboren in: "+jaar);
+jaar = prompt("Wanneer ben jij geboren?");
+jaar = jaar+5;
+
+alert("In"+ jaar + " was jij 5 jaar");
