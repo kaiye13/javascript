4781e948978a18549983e4c6d167754f0c8596aa
kaiye13 <kaatschillemans@hotmail.com>
2020-02-14 14:03:23 +0100

    oefening 10 is geadd

---
+++ oef10/oefening10.js
@@ -0,0 +1,24 @@
+var getal;
+
+function leesGetal() {
+
+    do {
+        getal = +prompt("geef een getal in tussen 1 en 20:"); //een integer van maken 
+    } while (isNaN(getal) || getal == "" || getal == null || getal < 1 || getal > 20);
+    document.getElementById("waarde").innerHTML = "<h1> De tafels van" + getal + "</h1>";
+    return getal;
+}
+
+
+
+
+function toonTafels(getal) {
+    var total = "";
+    for (var table = 1; table <= 20; table++) {
+        total += "<p>" + table + " * " + getal + " = " + (table * getal) + "</p>";
+    }
+    return document.getElementById("resultaat").innerHTML = total;
+}
+
+leesGetal();
+toonTafels(getal);
